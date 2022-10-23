684ec823b3cefac5fa427e4bf74fe95a6e76dad6
Juanque <juan.castro.arancibia@gmail.com>
2022-10-22 23:47:52 -0300

    Fixes styles all styles in home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,124 @@
 </script>
 
 <!-- Home content -->
-<section>
-  <div class="profile_img">
-    <div class="column" />
+<section
+  class="
+  flex justify-center 
+  flex-col sm:flex-row 
+  items:center sm:items-start
+  mt-[10vh]
+  "
+>
+  <div class="sm:ml-10 flex justify-center">
+    <div
+      class="
+      absolute bg-secondary top-0 h-screen
+      sm:z-0 w-32 sm:w-40"
+    />
     <Character />
   </div>
-  <header class="profile_txt">
-    <div class="titles">
-      <div class="row" />
-      <h1>Juan</h1>
-      <h2>Castro Arancibia</h2>
-      <h3>UX/UI Designer | Full-stack web developer</h3>
+  <header
+    class="w-screen flex flex-col text-center justify-center sm:text-left sm:block sm:w-[600px] sm:ml-[5vw]"
+  >
+    <div class="ubuntu text-shadow text-white selection:bg-secondary selection:text-gray-700">
+      <div
+        class="
+        absolute bg-primary w-screen left-0
+        h-[8.5rem] sm:h-36 lg:h-48"
+      />
+      <h1
+        class="
+        relative z-10 font-bold
+        text-[3rem] sm:text-[4rem] lg:text-[6rem]
+        leading-[3.25rem] sm:leading-[4.6rem] lg:leading-[7rem] "
+      >
+        Juan
+      </h1>
+      <h2
+        class="
+        relative font-bold z-10
+        mb-2 sm:mb-1 sm:-mt-2 lg:mb-0 lg:-mt-4 
+        text-[2rem] lg:text-[3rem] 
+        leading-[2.25rem] lg:leading-[3.25rem]
+       "
+      >
+        Castro Arancibia
+      </h2>
+      <h3
+        class="
+        relative z-10 
+        sm:text-[1rem] lg:text-[1.5rem]
+        mb-4 sm:mb-8
+        "
+      >
+        UX/UI Designer | Full-stack web developer
+      </h3>
     </div>
-    <p>
-      "Desde que era muy chico siempre me gusto jugar creando cosas. Empecé a usar Photoshop cuando
+    <p
+      class="
+        hidden sm:block sm:w-[50vw] space-mono mb-8 leading-8
+        selection:text-white selection:bg-primary
+      "
+    >
+      Desde que era muy chico siempre me gusto jugar creando cosas. Empecé a usar Photoshop cuando
       tenía 13 y cree mi primer web con HTML y CSS cuando tenia 15. Después empecé a aprender
       Javascript y hoy tengo experiencia con ReactJS, NodeJS, Flutter, Python, Firebase y
-      Kubernetes, entre otras tecnologías. También me encanta hacer música, sacar fotos y dibujar."
+      Kubernetes, entre otras tecnologías. También me encanta hacer música, sacar fotos y dibujar.
     </p>
-    <a class="resume_link" target="_blank" href="./Juan_Castro_Arancibia_CVes.pdf"
-      ><button class="cta">DESCARGAR CV</button></a
-    >
-    <a href="mywork.html"><button class="cta primary">MIRÁ MI TRABAJO</button></a>
+    <a target="_blank" href="./Juan_Castro_Arancibia_CVes.pdf">
+      <button
+        class="
+          cta ubuntu font-bold
+          cursor-pointer relative 
+          bg-primary text-sm text-white
+          my-4 sm:my-0 p-[8px] mr-4 rounded-[5px]
+          border-[1px]
+          border-primary-dark sm:border-transparent hover:border-white
+        "
+      >
+        CURRICULUM VITAE
+      </button>
+    </a>
+    <a href="mywork.html">
+      <button
+        class="
+          cta-2 ubuntu font-bold
+          cursor-pointer relative 
+          bg-secondary text-sm text-white
+          my-4 sm:my-0 p-[8px] mr-4 rounded-[5px]
+          border-[1px]
+          border-secondary-dark sm:border-transparent hover:border-white
+        "
+      >
+        MIRÁ MI TRABAJO
+      </button>
+    </a>
   </header>
 </section>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap');
+
+  .ubuntu {
+    font-family: 'Ubuntu', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+      Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  .space-mono {
+    font-family: 'Space mono', monospace;
+  }
+
+  .text-shadow {
+    text-shadow: 2px 2px rgba(0, 0, 0, 0.2);
+  }
+
+  .cta {
+    box-shadow: 2px 2px 0 #ad414c;
+    text-shadow: 0 0 1px rgba(0, 0, 0, 0.4);
+  }
+
+  .cta-2 {
+    box-shadow: 2px 2px 0 #6fa9a7;
+    text-shadow: 0 0 1px rgba(0, 0, 0, 0.4);
+  }
+</style>
--- src/routes/home/Character.svelte
+++ src/routes/home/Character.svelte
@@ -1,17 +1,20 @@
 <script>
-  import { fly } from 'svelte/transition'
   import Bulb from './character/Bulb.svelte'
   import Smile from './character/Smile.svelte'
   import Body from './character/Body.svelte'
   import Eyes from './character/Eyes.svelte'
   let active = false
-  let showEyes = false
 
   let borders = true
 </script>
 
 <svg
-  class="w-60 h-60 object-contain -mt-4 shadow-md bg-white cursor-pointer z-10 rounded-full"
+  class="
+    z-30 sm:z-10
+    object-contain bg-white cursor-pointer
+    rounded-full shadow-md
+    w-40 h-40 sm:w-48 sm:h-48 lg:w-60 lg:h-60 
+    mb-4 sm:mb-0 -mt-4"
   viewBox="0 0 373 512"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
@@ -19,52 +22,9 @@
   on:mouseleave={() => (active = false)}
 >
   <Body {borders} />
-  <Eyes eyes={showEyes} {active} />
+  <Eyes {active} {borders} />
   {#if active}
     <Bulb {active} {borders} />
   {/if}
-  <Smile active={true} {borders} />
+  <!-- <Smile {active} {borders} /> -->
 </svg>
-
-<style>
-  .picture #light_bulb {
-    opacity: 0;
-    transition: 0.3s ease-out;
-    position: relative;
-  }
-
-  .picture #eyes_active {
-    opacity: 0;
-    animation: blink 3s linear infinite normal forwards;
-  }
-  .picture #eyes {
-    opacity: 0;
-    animation: blink_close 3s linear infinite normal forwards;
-  }
-
-  .picture:hover #eyes {
-    opacity: 0;
-    animation: none;
-  }
-
-  .picture:hover #eyes_active {
-    animation: none;
-    opacity: 1;
-  }
-
-  .picture:hover #light_bulb {
-    opacity: 1;
-    transform: translateY(-50px);
-  }
-
-  .profile_img .picture #smile {
-    opacity: 0;
-    transform: translateY(-10px);
-    transition: 0.3s ease;
-  }
-
-  .profile_img .picture:hover #smile {
-    opacity: 1;
-    transform: scale(1);
-  }
-</style>
